57c652308746e30f5139e157cc49ec933bd414a3
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-01-22 18:15:14 -0700

    Generated necessary files for project. Created tables for department, role and employee.

---
+++ Main/db/seed.sql
@@ -0,0 +1,18 @@
+USE employee_db;
+INSERT INTO department {
+    (1, "Engineering"),
+    (2, "Human Resources")
+    (3, "Research & Development")
+}
+
+INSERT INTO role {
+    (1, "Engineer", 120000, 1),
+    (2, "Accounting", 80000, 2),
+    (3, "Chief Engineer", 150000, 3)
+}
+
+INSERT INTO employee {
+    (1, "Jerry", "Shaw", 1, null),
+    (2, "Vincent", "Chase", 2, 1),
+    (3, "Ricky". "Jerret", 3, 1)
+}
